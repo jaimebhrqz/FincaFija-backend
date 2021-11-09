b757676275356dc6fbfd0a010f9f77d161e8b35a
INGJAIMEBHRQZ\usuario <jaimebhrqz@gmail.com>
2021-11-08 22:22:42 -0500

    incluye cambios 08/1172021

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('Prueba del servidor');
 });
