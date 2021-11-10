cd0b0336f0385b605cee7dcb0ae97ca163da66e4
INGJAIMEBHRQZ\usuario <jaimebhrqz@gmail.com>
2021-11-09 21:51:08 -0500

    9/11/2021 agregar service, routes

---
+++ src/services/mysql.service.ts
@@ -0,0 +1,9 @@
+const getConnection = () => {
+    // codigo para hacer la conexion
+}
+
+const executeQuery = (query: string) => {
+    //codigo para ejecutar el query
+}
+
+export default executeQuery;
